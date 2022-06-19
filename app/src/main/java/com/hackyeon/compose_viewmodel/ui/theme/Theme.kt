36edc06a6521571a4ea0782d9a1ca3dcfc6ae0dd
hackyeon <skek933@gmail.com>
2022-06-19 16:57:51 +0900

    recommit

--- app/src/main/java/com/hackyeon/compose_viewmodel/ui/theme/Theme.kt
+++ app/src/main/java/com/hackyeon/compose_viewmodel/ui/theme/Theme.kt
@@ -1,19 +1,4 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.codelabs.state.ui.theme
+package com.hackyeon.compose_viewmodel.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -43,7 +28,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun BasicStateCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun Compose_viewModelTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
